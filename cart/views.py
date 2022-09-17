eaa35addc2169f69b47f1108434b608935e6f9e1
bruzgus1 <poviliukas987412365@gmail.com>
2022-09-17 13:23:12 +0000

    4.8) added notification messages

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+from products.models import Product
 
 # Create your views here.
 
@@ -38,6 +40,7 @@ def update_cart(request, item_id):
 def add_to_cart(request, item_id):
     """ Add a quantity of the specified product to the shopping cart """
 
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     tier = None
@@ -58,6 +61,7 @@ def add_to_cart(request, item_id):
             cart[item_id] += quantity
         else:
             cart[item_id] = quantity
+            messages.success(request, f'Added {product.name} to your bag')
 
     request.session['cart'] = cart
     return redirect(redirect_url)
