5819d292f2a4911ee404e123cb00ceda3ee0dd84
bruzgus1 <poviliukas987412365@gmail.com>
2022-09-21 20:51:13 +0000

    4.8) completed webhook handler Part 1 (got a bug)

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -72,6 +72,7 @@ form.addEventListener('submit', function (ev) {
             payment_method: {
                 card: card,
                 billing_details: {
+                    username: $.trim(form.username.value),
                     name: $.trim(form.full_name.value),
                     phone: $.trim(form.phone_number.value),
                     email: $.trim(form.email.value),
