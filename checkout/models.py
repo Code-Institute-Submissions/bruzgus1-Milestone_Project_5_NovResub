42e80fa071421adb9e73a2814c827b7ef8c3d5a5
bruzgus1 <poviliukas987412365@gmail.com>
2022-09-21 11:06:56 +0000

    4.8) Added checkout success logic

--- checkout/models.py
+++ checkout/models.py
@@ -32,7 +32,7 @@ class Order(models.Model):
         """
         Update grand total each time a line item is added.
         """
-        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
         self.grand_total = self.order_total
         self.save()
 
@@ -52,7 +52,7 @@ class Order(models.Model):
 class OrderLineItem(models.Model):
     order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
     product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
-    product_tier = models.CharField(max_length=2, null=True, blank=True) # 1-11
+    product_tier = models.CharField(max_length=2, null=True, blank=True)  # 1-11
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
 
