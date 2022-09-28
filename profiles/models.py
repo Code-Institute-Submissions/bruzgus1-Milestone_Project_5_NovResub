d6a55579cfe07961177c6fd14073551268b4b593
bruzgus1 <poviliukas987412365@gmail.com>
2022-09-28 17:50:32 +0000

    2.5) Added profile form, views and updated template

--- profiles/models.py
+++ profiles/models.py
@@ -13,7 +13,7 @@ class UserProfile(models.Model):
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     regular_phone_number = models.CharField(max_length=20, null=True, blank=True)
-    regular_country = CountryField(blank_label='Country *', null=True, blank=True)
+    regular_country = CountryField(blank_label='Country', null=True, blank=True)
     regular_postcode = models.CharField(max_length=20, null=True, blank=True)
     regular_town_or_city = models.CharField(max_length=40, null=True, blank=True)
     regular_street_address1 = models.CharField(max_length=80, null=True, blank=True)
