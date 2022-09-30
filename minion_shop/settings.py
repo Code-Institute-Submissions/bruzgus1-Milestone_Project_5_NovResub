2a00c0d3fe3f44acf51825b7e437ad9b978cd30b
bruzgus1 <poviliukas987412365@gmail.com>
2022-09-30 09:48:00 +0000

    added sitemap.xml and robot.txt files, set Debug to False

--- minion_shop/settings.py
+++ minion_shop/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')  # Changed django secret key
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['bruzgus1minion-shop.herokuapp.com', 'localhost']
 
